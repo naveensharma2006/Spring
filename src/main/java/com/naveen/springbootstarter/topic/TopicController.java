52a09c24f214dc2e55f5380f5a497262bc9da32a
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-05-14 18:44:45 +0530

    Pushed

--- src/main/java/com/naveen/springbootstarter/topic/TopicController.java
+++ src/main/java/com/naveen/springbootstarter/topic/TopicController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author naveensharma
+ *
+ */
 @RestController
 public class TopicController {
 
